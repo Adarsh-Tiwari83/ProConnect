e5e43c79529e1517db3432f1c13c1911cf297344
Adarsh Tiwari <tadarsh249@gmail.com>
2024-05-27 15:45:26 +0530

    final merge

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Main from './components/Main'
+import { Route, Routes } from 'react-router-dom'
+import Signin from './components/Signin'
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Signin/>}/>
+        <Route path='/main' element={<Main/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
